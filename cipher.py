719115713067479b96861ba5a8bda9a89c9b3779
ChandanT21 <vichandan@me.com>
2023-07-15 22:44:03 -0400

    Combined encrypt and decrypt methods into 1.

--- cipher.py
+++ cipher.py
@@ -18,36 +18,15 @@ def launch():
 
 def execute(mode, message, shift):
     cipher = ''
-    if mode == 'encode':
-        cipher = encrypt(message, shift)
-    if mode == 'decode':
-        cipher = decrypt(message, shift)
+    for letter in message:
+        if letter in alphabet:
+            index = alphabet.index(letter)
+            if mode == 'encode':
+                cipher_index = index + shift
+            else:
+                cipher_index = index - shift
+            cipher_index = cipher_index % len(alphabet)
+            cipher += alphabet[cipher_index]
+        else:
+            cipher += letter
     print(f"Here's the {mode}d result:  {cipher}")
-
-
-def encrypt(message, shift):
-    cipher = ''
-    for letter in message:
-        if letter in alphabet:
-            index = alphabet.index(letter)
-            cipher_index = index + shift
-            if cipher_index > len(alphabet):
-                cipher_index = cipher_index % len(alphabet)
-            cipher += alphabet[cipher_index]
-        else:
-            cipher += letter
-    return cipher
-
-
-def decrypt(message, shift):
-    cipher = ''
-    for letter in message:
-        if letter in alphabet:
-            index = alphabet.index(letter)
-            cipher_index = index - shift
-            if cipher_index < 0:
-                cipher_index = cipher_index % len(alphabet)
-            cipher += alphabet[cipher_index]
-        else:
-            cipher += letter
-    return cipher
