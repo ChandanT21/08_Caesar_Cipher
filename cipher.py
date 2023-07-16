4cd5136b7615b80bbac92a23315c77ef67b3a73f
ChandanT21 <vichandan@me.com>
2023-07-15 22:31:29 -0400

    Updated rerun logic.

--- cipher.py
+++ cipher.py
@@ -4,17 +4,13 @@ alphabet = list(string.ascii_lowercase)
 
 
 def launch():
-    active = True
-    while active:
-        mode = input("Type 'encode' to encrypt the message or type 'decode' to decrypt a message.\n").lower()
-        if mode == 'encode' or mode == 'decode':
-            message = input("Type your message: \n").lower()
-            shift_no = int(input("Type the shift number: \n"))
-            execute(mode, message, shift_no)
-            if (input("Type 'yes' if you want to go again, otherwise type 'no'.\n")) == 'no':
-                active = False
-        else:
-            print("Incorrect mode selected. Try again.")
+    mode = input("Type 'encode' to encrypt the message or type 'decode' to decrypt a message.\n").lower()
+    if mode == 'encode' or mode == 'decode':
+        message = input("Type your message: \n").lower()
+        shift_no = int(input("Type the shift number: \n"))
+        execute(mode, message, shift_no)
+    else:
+        print("Incorrect mode selected. Try again.")
 
 
 def execute(mode, message, shift):
@@ -29,26 +25,26 @@ def execute(mode, message, shift):
 def encrypt(message, shift):
     cipher = ''
     for letter in message:
-        if letter == " ":
-            cipher += " "
-        else:
+        if letter in alphabet:
             index = alphabet.index(letter)
             cipher_index = index + shift
             if cipher_index > len(alphabet):
                 cipher_index = cipher_index % len(alphabet)
             cipher += alphabet[cipher_index]
+        else:
+            cipher += letter
     return cipher
 
 
 def decrypt(message, shift):
     cipher = ''
     for letter in message:
-        if letter == " ":
-            cipher += " "
-        else:
+        if letter in alphabet:
             index = alphabet.index(letter)
             cipher_index = index - shift
             if cipher_index < 0:
                 cipher_index = cipher_index % len(alphabet)
             cipher += alphabet[cipher_index]
+        else:
+            cipher += letter
     return cipher
--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 import display
 import cipher
 
-
 if __name__ == '__main__':
     print(display.logo)
-    cipher.launch()
+    active = True
+    choices = ['yes', 'no']
+    while active:
+        cipher.launch()
+        rerun = input("Type 'yes' if you want to go again, otherwise type 'no'.\n").lower()
+        while rerun not in choices:
+            rerun = input("Incorrect option stated. Please type 'yes' to go again or 'no' to quit. ")
+        if rerun == 'no':
+            active = False
+        else:
+            active = True
